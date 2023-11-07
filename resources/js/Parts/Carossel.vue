8b7b5dc5252ee2ab4291719c6bcc587ae3e0e098
ELfassiMohamed <elfassimohamed2001@gmail.com>
2023-11-07 21:27:28 +0100

    11/7/2023

---
+++ resources/js/Parts/Carossel.vue
@@ -0,0 +1,28 @@
+<template>
+    <Carousel :autoplay="2000" :wrap-around="true">
+      <Slide v-for="slide in 10" :key="slide">
+        <div class="carousel__item">{{ slide }}</div>
+      </Slide>
+  
+      <template #addons>
+        <Pagination />
+      </template>
+    </Carousel>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue'
+  import { Carousel, Pagination, Slide } from 'vue3-carousel'
+  
+  import 'vue3-carousel/dist/carousel.css'
+  
+  export default defineComponent({
+    name: 'Autoplay',
+    components: {
+      Carousel,
+      Slide,
+      Pagination,
+    },
+  })
+  </script>
+  
---
+++ resources/js/Parts/Navbar.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div class="bg-white">
+    <!-- Mobile menu -->
+    
+    <header class="relative bg-white">
+      <p class="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">Get free delivery on orders over $100</p>
+    </header>
+  </div>
+</template>
+
+<script setup>
+
+</script>
